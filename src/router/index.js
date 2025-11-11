4ac1b6642e78b7bb370a136ff0f26d2164d34c85
Phan Ngọc Bảo Vinh <pv198357@gmail.com>
2025-11-11 22:07:58 +0700

    install tailwindcss

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,7 @@
 // src/router/index.ts
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import About from '../views/About.vue'
 
 const routes = [
-  { path: '/', name: 'Home', component: Home },
-  { path: '/about', name: 'About', component: About },
 ]
 
 const router = createRouter({
