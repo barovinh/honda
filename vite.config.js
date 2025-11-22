3838ca613708a3eaa3b7f88486c42f05a5e9d81d
Phan Ngọc Bảo Vinh <pv198357@gmail.com>
2025-11-22 14:57:07 +0700

    change vite

--- vite.config.js
+++ vite.config.js
@@ -10,10 +10,6 @@ export default defineConfig({
   // Base path for the built assets. If you host at https://<user>.github.io/<repo>/
   // set this to '/<repo>/'. For local or root domain use '/'.
   base: '/hondaTienGiang/',
-  plugins: [tailwindcss(), vue(), vueDevTools()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+  plugins: [vue(), vueDevTools(), tailwindcss()],
+  resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } }
 })
