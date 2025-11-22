8ee78a6c1bfcfa76bd5cf7e0d000c44a7eac0abf
Phan Ngọc Bảo Vinh <pv198357@gmail.com>
2025-11-22 14:41:41 +0700

    web hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,3 @@
-// src/router/index.ts
 import Index from '@/pages/index/index.vue'
 import Oto from '@/pages/oto/index.vue'
 import Contact from '@/pages/contact/index.vue'
@@ -8,13 +7,12 @@ const routes = [
   { path: '/', name: 'index', component: Index },
   { path: '/oto', name: 'oto', component: Oto },
   { path: '/contact', name: 'contact', component: Contact },
-  // fallback: redirect unknown paths to index
   { path: '/:pathMatch(.*)*', redirect: '/' }
 ]
 
 const router = createRouter({
-  // Use hash history to avoid server-side 404s on GitHub Pages
-  history: createWebHistory('/hondaTienGiang/'), // <-- base path phải trùng với vite.config.js
+  // Dùng hash history để tránh lỗi refresh trên GitHub Pages
+  history: createWebHashHistory(),
   routes
 })
 
